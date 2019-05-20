c9c0023ae58f0bca1e24ff01a8bfbd1af6bd64d8
Igumnov Alexey <igumnov_aleksei@mail.ru>
2019-05-20 15:50:01 +0400

    third lesson ready

--- app/src/main/java/ru/madbrains/listexample/CatList.kt
+++ app/src/main/java/ru/madbrains/listexample/CatList.kt
@@ -8,30 +8,36 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 
-class CatAdapter(private val catTexts: List<Cat>) : RecyclerView.Adapter<CatViewHolder>() {
+class CatAdapter(private val cats: List<Cat>) : RecyclerView.Adapter<CatViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CatViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.cat_item, parent, false)
-
-        return CatViewHolder(view)
+        //создаем view из файла интерфейса
+        val rootView = LayoutInflater
+            .from(parent.context)
+            .inflate(R.layout.cat_item, parent, false)
+        return CatViewHolder(rootView)
     }
 
     override fun getItemCount(): Int {
-        return catTexts.size
+        return cats.size
     }
 
     override fun onBindViewHolder(holder: CatViewHolder, position: Int) {
-        holder.bind(catTexts.get(position))
+        holder.bind(cats.get(position))
     }
 }
 
 class CatViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
-    private val imageView = itemView.findViewById<ImageView>(R.id.imageViewId)
-    private val textView = itemView.findViewById<TextView>(R.id.textViewId)
+    //находим ImageView
+    private val imageView: ImageView = itemView.findViewById(R.id.imageViewId)
+
+    //Находим TextView
+    private val textView: TextView = itemView.findViewById(R.id.textViewId)
 
     fun bind(cat: Cat) {
-        textView.text = cat.text
+        textView.text = cat.text //загружаем текст в TextView
+
         Glide.with(itemView).load(cat.image).into(imageView)
     }
 }
