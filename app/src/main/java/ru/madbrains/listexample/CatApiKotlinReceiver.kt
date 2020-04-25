34e2b5196074c0ace786ebae4b1f0bc72c2e7772
Alexey Igumnov <gum1985@gmail.com>
2020-04-25 18:31:28 +0400

    get text from server case without android

--- app/src/main/java/ru/madbrains/listexample/CatApiKotlinReceiver.kt
+++ app/src/main/java/ru/madbrains/listexample/CatApiKotlinReceiver.kt
@@ -3,6 +3,6 @@ package ru.madbrains.listexample
 import java.net.URL
 
 fun main() {
-    val catFactsContent = URL("https://api.myjson.com/bins/w4bj6").readText()
+    val catFactsContent = URL("https://raw.githubusercontent.com/gumam/android_course/master/db.json").readText()
     print(catFactsContent)
 }
