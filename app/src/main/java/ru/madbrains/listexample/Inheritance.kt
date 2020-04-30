bd2377d547b3817fbb24f40326f38556571fb81e
Alexey Igumnov <gum1985@gmail.com>
2020-04-30 10:29:25 +0400

    inheritance

---
+++ app/src/main/java/ru/madbrains/listexample/Inheritance.kt
@@ -0,0 +1,19 @@
+package ru.madbrains.listexample
+
+open class Chord //объявление корневого класса
+
+open class Mammal : Chord() { //объявление наследника
+    open fun legs(): Int { return 4 } //объявление метода
+}
+
+open class Primate : Mammal() {
+    override fun legs(): Int { return 2 } //переопределение метода (изменение свойств)
+    fun hands(): Int { return 2 } //объявление метода
+}
+
+open class Human (val name: String): Primate()
+
+fun main () {
+    val alexey = Human("Alexey") //создание объекта определенного класса
+    print(alexey.legs()) //использование объекта
+}
